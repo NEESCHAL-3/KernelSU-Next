b07a7695920b1b3f02fa25268b7dae9f6f5b7f5c
NEESCHAL-3 <Nischalkhadka022@gmail.com>
2026-07-20 21:16:16 +0545

    kernelsu: call exported SELinux helpers directly

--- kernel/feature/selinux_hide.c
+++ kernel/feature/selinux_hide.c
@@ -474,8 +474,8 @@ static void __nocfi type_attribute_bounds_av(struct policydb *policydb, struct c
     avd->allowed &= ~masked;
 
     /* audit masked permissions */
-    if (security_dump_masked_av_fn)
-        security_dump_masked_av_fn(policydb, scontext, tcontext, tclass, masked, "bounds");
+    security_dump_masked_av_fn(policydb, scontext, tcontext,
+                               tclass, masked, "bounds");
 }
 
 /*
@@ -791,11 +791,8 @@ void __nocfi security_compute_av_user_with_policy(struct selinux_policy *policy,
         goto out;
     }
 
-    if (context_struct_compute_av_fn) {
-        context_struct_compute_av_fn(policydb, scontext, tcontext, tclass, avd, NULL);
-    } else {
-        context_struct_compute_av(policydb, scontext, tcontext, tclass, avd, NULL);
-    }
+    context_struct_compute_av_fn(policydb, scontext, tcontext,
+                                 tclass, avd, NULL);
 out:
     return;
 allow:
